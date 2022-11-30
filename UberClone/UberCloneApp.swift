eee6505e7ffcba436521ccefd84c6cedcca0fa29
zhaslan057 <z.tanerbergenov@gmail.com>
2022-11-30 15:28:42 +0600

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Жаслан Танербергенов on 30.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
